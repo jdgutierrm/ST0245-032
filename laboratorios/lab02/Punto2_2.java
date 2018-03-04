a4ffb044b7c9b25accb498343a1d94f9c8afbc36
jvanegase <35736566+jvanegase@users.noreply.github.com>
2018-03-04 14:37:43 -0500

    Update Punto2_2.java

--- laboratorios/lab02/Punto2_2.java
+++ laboratorios/lab02/Punto2_2.java
@@ -1,76 +1,87 @@
 public class Punto2_2 {
     public int maxSpan(int[] nums) {
-        if (nums.length > 0) {
-            int maxSpan = 1;
-            for (int i = 0; i < nums.length; i++){
-                for (int j = nums.length - 1; j > i; j--){
-                    if (nums[j] == nums[i]) {
-                        int count = (j - i) + 1;
-                        if (count > maxSpan){
-                            maxSpan = count;
-                        }
-                    break;
-                    }
-                }
-            }
-            return maxSpan;
-        } else {
-            return 0;
-        }
+      int max=0;
+      int retador=0;
+      for(int i=0;i<nums.length;i++){                             
+      int j=nums.length-1;
+      while(nums[j]!=nums[i]){                                   
+      j--;
     }
-    public int[] fix34(int[] nums) {
-        for (int i = 0; i < nums.length; i++){
-            if (nums[i] == 3) {
-                int temp = nums[i + 1];
-                nums[i + 1] = 4;
-                for (int j = i + 2; j < nums.length; j++){
-                    if (nums[j] == 4){
-                        nums[j] = temp;
-                    }
-                }
-            }
-        }
-        return nums;
-    }
-    public int[] fix45(int[] nums) {
-        for (int i = 0; i < nums.length; i++){
-            if (nums[i] == 5 && i == 0 || nums[i] == 5 && nums[i - 1] != 4) {
-                int pos5 = i;
-                for (int j = 0; j < nums.length; j++){
-                    if (nums[j] == 4 && nums[j + 1] != 5) {
-                        int temp = nums[j + 1];
-                        nums[j + 1] = 5;
-                        nums[pos5] = temp;
-                    break;
-                    }
-                }
-            }
-        }
-        return nums;
-    }
-    public boolean canBalance(int[] nums) {
-        for (int i = 0; i < nums.length; i++) { 
-            int sum = 0;
-            for (int j = 0; j < i; j++) sum += nums[j];
-            for (int j = i; j < nums.length; j++) sum -= nums[j];
-                if (sum == 0){
-                    return true;
-                }
-        }
-        return false;
-    }
-    public boolean linearIn(int[] outer, int[] inner) {
-        int indexInner = 0;
-        int indexOuter = 0;
-        while (indexInner < inner.length && indexOuter < outer.length) {
-            if (outer[indexOuter] == inner[indexInner]) {
-                indexOuter++;
-                indexInner++;
-            } else {
-                indexOuter++;
-            }
-        }
-        return (indexInner == inner.length);
-    }
+     retador=j-i+1;
+     if(retador>max)max=retador;
+  }
+  return max;
 }
+
+    public int[] fix34(int[] nums) {
+      int temp=0;
+      boolean three=false;
+      for(int i=0;i<nums.length-1;i++){                         
+      if(nums[i]==3)
+        three=true;
+      if(three==true){
+        three=false;
+      if(nums[i+1]!=4){
+      for(int j=1;j<nums.length;j++){                         
+        if(nums[j]==4&&nums[j-1]!=3){
+            temp=nums[i+1];
+          nums[i+1]=nums[j];
+          nums[j]=temp;
+        }
+      }
+      }  
+      }
+    }
+
+    return nums;
+}
+
+   public int[] fix45(int[] nums) {
+      for(int i=0;i<nums.length;i++){                         
+      if(nums[i]==4){
+      for(int j=0;j<nums.length;j++){                    
+        if(nums[j]==5){
+          if(j>0&&nums[j-1]!=4){
+            int t=nums[i+1];
+            nums[i+1]=5;
+            nums[j]=t;
+          }
+          else if(j==0){
+            int t=nums[i+1];
+            nums[i+1]=5;
+            nums[j]=t;
+          }
+        }
+      }
+    }
+  }
+  return nums;
+}
+
+    public boolean linearIn(int[] outer, int[] inner) {
+      int var=0;
+      for(int j=0;j<inner.length;j++){                                             
+      for(int i=0;i<outer.length;i++){                                          
+      if(inner[j]==outer[i]){
+          var++;
+          break;
+      }
+    }
+  }
+    if(var==inner.length) return true;
+  return false;
+}
+
+   public int[] seriesUp(int n) {
+     int[]arr=new int[n*(n+1)/2];
+     int l=0;
+     for(int i=1;i<=n;i++){                                        
+     for(int j=1;j<=i;j++,l++){                            
+      arr[l]=j;
+  }
+
+}
+ return arr;
+}
+
 
